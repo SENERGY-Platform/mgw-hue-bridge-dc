996c4ec07553bd99fa438014e620fd38cd8845c7
y-du <42994541+y-du@users.noreply.github.com>
2022-01-26 14:57:25 +0100

    query lights and sensors, change data structure, handle meta data and data changes

--- hue_bridge/monitor.py
+++ hue_bridge/monitor.py
@@ -53,43 +53,47 @@ class Monitor(threading.Thread):
         while True:
             if self.__refresh_flag:
                 self.__refresh_devices(self.__refresh_flag)
-            queried_devices = self.__queryBridge()
+            queried_devices = self.__queryBridge(("lights", "sensors"))
             if queried_devices:
                 self.__evaluate(queried_devices)
             time.sleep(self.__query_delay)
 
-    def __queryBridge(self):
-        try:
-            resp = requests.get(
-                "https://{}/{}/{}/lights".format(self.__hue_bridge.host, conf.Bridge.api_path, conf.Bridge.api_key),
-                verify=False,
-                timeout=conf.Discovery.timeout
-            )
-            if resp.ok:
-                data = resp.json()
-                devices = dict()
-                for number, device in data.items():
-                    try:
-                        devices["{}{}".format(conf.Discovery.device_id_prefix, device["uniqueid"])] = (
-                            {
-                                "name": device["name"],
-                                "model": device["modelid"],
-                                "number": number,
-                                "info": device["state"]
-                            },
-                            {
-                                "product_name": device["productname"],
-                                "manufacturer": device["manufacturername"],
-                                "product_type": device["type"]
-                            }
-                        )
-                    except KeyError as ex:
-                        logger.error("could not parse device - {}\n{}".format(ex, device))
-                return devices
-            else:
-                raise RuntimeError(resp.status_code)
-        except Exception as ex:
-            logger.error("could not query bridge - '{}'".format(ex))
+    def __queryBridge(self, apis):
+        devices = dict()
+        for api in apis:
+            try:
+                resp = requests.get(
+                    f"https://{self.__hue_bridge.host}/api/{self.__hue_bridge.api_key}/{api}",
+                    verify=False,
+                    timeout=self.__request_timeout
+                )
+                if resp.ok:
+                    resp = resp.json()
+                    for number, device in resp.items():
+                        try:
+                            if "uniqueid" in device:
+                                devices["{}{}".format(self.__device_id_prefix, device["uniqueid"])] = {
+                                    "meta_data": {
+                                        "name": device["name"],
+                                        "model_id": device["modelid"],
+                                        "type": device["type"],
+                                        "manufacturer_name": device["manufacturername"],
+                                        "sw_version": device["swversion"],
+                                        "number": number,
+                                        # "api": api
+                                    },
+                                    "data": {
+                                        "state": device.get("state") or {},
+                                        "config": device.get("config") or {}
+                                    }
+                                }
+                        except KeyError as ex:
+                            logger.error("could not parse device - {}\n{}".format(ex, device))
+                else:
+                    raise RuntimeError(resp.status_code)
+            except Exception as ex:
+                logger.error("could not query bridge - '{}'".format(ex))
+        return devices
 
     def __handle_missing_device(self, device_id: str):
         try:
@@ -112,9 +116,9 @@ class Monitor(threading.Thread):
         try:
             device = Device(
                 id=device_id,
-                type=data[1]["product_type"],
+                type=self.__type_map[data["meta_data"]["type"]],
                 bridge=self.__hue_bridge,
-                **data[0]
+                **data
             )
             logger.info("found '{}' with id '{}'".format(device.name, device_id))
             self.__mqtt_client.publish(
@@ -127,26 +131,41 @@ class Monitor(threading.Thread):
         except Exception as ex:
             logger.error("can't add '{}' - {}".format(device_id, ex))
 
-    def __handle_changed_device(self, device_id: str, data: dict):
+    def __handle_changed_meta_data(self, device_id: str, data: dict):
         try:
             device = self.__device_pool[device_id]
-            backup = dict(device)
-            device.name = data["name"]
-            device.model = data["model"]
-            device.number = data["number"]
-            device.info = data["info"]
-            if backup["name"] != data["name"] or backup["info"]["reachable"] != data["info"]["reachable"]:
-                try:
+            meta_data_bk = device.data.copy()
+            try:
+                device.meta_data = data
+                self.__mqtt_client.publish(
+                    topic=mgw_dc.dm.gen_device_topic(self.__dc_id),
+                    payload=json.dumps(mgw_dc.dm.gen_set_device_msg(device)),
+                    qos=1
+                )
+            except Exception as ex:
+                device.meta_data = meta_data_bk
+                raise ex
+        except Exception as ex:
+            logger.error("can't update '{}' - {}".format(device_id, ex))
+
+    def __handle_changed_data(self, device_id: str, data: dict):
+        try:
+            device = self.__device_pool[device_id]
+            data_bk = device.data.copy()
+            state_bk = device.state
+            try:
+                device.data = data
+                if state_bk != device.state:
                     self.__mqtt_client.publish(
-                        topic=mgw_dc.dm.gen_device_topic(conf.Client.id),
+                        topic=mgw_dc.dm.gen_device_topic(self.__dc_id),
                         payload=json.dumps(mgw_dc.dm.gen_set_device_msg(device)),
                         qos=1
                     )
-                except Exception as ex:
-                    device.name = backup["name"]
-                    data["info"]["reachable"] = backup["info"]["reachable"]
-                    device.info = data["info"]
-                    raise ex
+                if data_bk["state"] != device.data["state"]:
+                    pass
+            except Exception as ex:
+                device.data = data_bk
+                raise ex
         except Exception as ex:
             logger.error("can't update '{}' - {}".format(device_id, ex))
 
@@ -155,21 +174,25 @@ class Monitor(threading.Thread):
         unknown_set = set(unknown)
         missing = known_set - unknown_set
         new = unknown_set - known_set
-        changed = {key for key in known_set & unknown_set if dict(known[key]) != unknown[key][0]}
-        return missing, new, changed
+        changed_meta_data = {key for key in known_set & unknown_set if known[key].meta_data != unknown[key]["meta_data"]}
+        changed_data = {key for key in known_set & unknown_set if known[key].data != unknown[key]["data"]}
+        return missing, new, changed_meta_data, changed_data
 
     def __evaluate(self, queried_devices):
         try:
-            missing_devices, new_devices, changed_devices = self.__diff(self.__device_pool, queried_devices)
+            missing_devices, new_devices, changed_meta_data, changed_data = self.__diff(self.__device_pool, queried_devices)
             if missing_devices:
                 for device_id in missing_devices:
                     self.__handle_missing_device(device_id)
             if new_devices:
                 for device_id in new_devices:
                     self.__handle_new_device(device_id, queried_devices[device_id])
-            if changed_devices:
-                for device_id in changed_devices:
-                    self.__handle_changed_device(device_id, queried_devices[device_id][0])
+            if changed_meta_data:
+                for device_id in changed_meta_data:
+                    self.__handle_changed_meta_data(device_id, queried_devices[device_id]["meta_data"])
+            if changed_data:
+                for device_id in changed_data:
+                    self.__handle_changed_data(device_id, queried_devices[device_id]["data"])
         except Exception as ex:
             logger.error("can't evaluate devices - {}".format(ex))
 
